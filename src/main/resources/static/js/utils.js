b286a39d623f4aa04aced5809a6e4f5c658779e7
xiangqians <shixiangqian.s@gmail.com>
2022-08-18 22:35:38 +0800

    静态页面修改

---
+++ src/main/resources/static/js/utils.js
@@ -0,0 +1,122 @@
+/**
+ * 工具类
+ * @constructor
+ */
+function Utils() {
+}
+
+/**
+ * 双击事件处理器
+ * @constructor
+ */
+Utils.DoubleClickEvent = function () {
+    this.clickCount = 0;
+    this.prevTimestamp = 0;
+    this.curTimestamp = 0;
+}
+/**
+ * 点击
+ * @param callback 双击事件回调函数
+ * @param interval 双击间隔，单位：ms
+ */
+Utils.DoubleClickEvent.prototype.click = function (callback, interval) {
+    if (interval == undefined) {
+        interval = 300;
+    }
+    const self = this;
+
+    // setTimeout
+    // _self.clickCount++;
+    // setTimeout(() => {
+    //     if (_self.clickCount === 2) {
+    //         callback();
+    //     }
+    //     _self.clickCount = 0;
+    // }, interval);
+
+    // timestamp
+    self.prevTimestamp = self.curTimestamp;
+    self.curTimestamp = new Date().getTime();
+    if (self.curTimestamp - self.prevTimestamp < interval) {
+        self.prevTimestamp = 0;
+        callback();
+    }
+
+}
+
+/**
+ * 获取请求参数
+ * @returns {null|Map<any, any>}
+ */
+Utils.getRequestParams = function () {
+    let url = location.href;
+    // console.log('url', url);
+    let index = url.indexOf("?");
+    if (index <= 0) {
+        return null;
+    }
+
+    let map = new Map();
+    let params = url.substring(index + 1, url.length).split('&');
+    for (let i = 0, length = params.length; i < length; i++) {
+        let nv = params[i].split('=');
+        let name = nv[0].trim();
+        let value = nv.length == 2 ? nv[1].trim() : null;
+        map.set(name, value);
+    }
+    return map;
+}
+
+/**
+ * 存储器
+ * @constructor
+ */
+Utils.Storage = function () {
+}
+
+Utils.Storage.set = function (name, value) {
+    Utils.Storage._storageStrategy.set(name, value);
+}
+
+Utils.Storage.get = function (name) {
+    return Utils.Storage._storageStrategy.get(name);
+}
+
+// session Storage
+// sessionStorage和localStorage的区别是：
+// 1、localStorage没有过期时间
+// 2、sessionStorage针对一个session进行数据存储，生命周期与session相同，当用户关闭浏览器后，数据将被删除。
+Utils.Storage._sessionStorage = function () {
+}
+
+Utils.Storage._sessionStorage.set = function (name, value) {
+    sessionStorage.setItem(name, value)
+}
+
+Utils.Storage._sessionStorage.get = function (name) {
+    return sessionStorage.getItem(name);
+}
+
+// cookie Storage
+Utils.Storage._cookieStorage = function () {
+}
+
+Utils.Storage._cookieStorage.set = function (name, value) {
+    document.cookie = name + "=" + value;
+}
+
+Utils.Storage._cookieStorage.get = function (name) {
+    let cookie = document.cookie;
+    let array = cookie.split(';');
+    for (let i = 0, length = array.length; i < length; i++) {
+        let kv = array[i].split('=');
+        if (name === kv[0].trim()) {
+            return kv.length >= 2 ? kv[1].trim() : null;
+        }
+    }
+    return null;
+}
+
+// 设置存储策略
+Utils.Storage._storageStrategy = Utils.Storage._sessionStorage;
+
