fc913b93a4be1e31561279d8bdd3aa4dca0c2430
xiangqians <shixiangqian.s@gmail.com>
2022-08-28 22:07:37 +0800

    @title

--- conf/bat/startup.bat
+++ conf/bat/startup.bat
@@ -1,3 +1,4 @@
+@title Asst
 @echo on
 java -Dfile.encoding=utf-8 -Xss4096K -Xms512M -Xmx512M -jar asst-2022.8.jar --server.address=127.0.0.1 --server.port=8080 --server.servlet.context-path=/
 pause
